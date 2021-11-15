55cf6054f64052fb470ecb51cec56aa1e6ba0858
2018yuli <l898974766@outlook.com>
2021-11-15 10:48:56 +0800

    20211115

---
+++ 933.最近的请求次数.java
@@ -0,0 +1,32 @@
+/*
+ * @lc app=leetcode.cn id=933 lang=java
+ *
+ * [933] 最近的请求次数
+ */
+
+// @lc code=start
+class RecentCounter {
+
+    // Solution from Nick White
+    Queue<Integer> queue;
+
+    public RecentCounter() {
+        queue = new LinkedList<Integer>();
+    }
+    
+    public int ping(int t) {
+        queue.add(t);
+        while (queue.peek() < t - 3000) {
+            queue.poll();
+        }
+        return queue.size();
+    }
+}
+
+/**
+ * Your RecentCounter object will be instantiated and called as such:
+ * RecentCounter obj = new RecentCounter();
+ * int param_1 = obj.ping(t);
+ */
+// @lc code=end
+
