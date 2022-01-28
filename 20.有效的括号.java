0632b9793236f2f84a5c938da4c39ee72d84fd96
2018yuli <l898974766@outlook.com>
2022-01-28 21:17:37 +0800

    20220128

--- 20.有效的括号.java
+++ 20.有效的括号.java
@@ -8,43 +8,46 @@
 class Solution {
     public boolean isValid(String s) {
         // mine
-        Stack<Character> stack = new Stack<Character>();
-        for (char c : s.toCharArray()) {
-            if (c == '(' || c == '{' || c == '[') {
-                stack.push(c);
-            }
-            if (c == ')' && (stack.isEmpty() || stack.pop() != '(')) {
-                return false;
-            }
-            if (c == '}' && (stack.isEmpty() || stack.pop() != '{')) {
-                return false;
-            }
-            if (c == ']' && (stack.isEmpty() || stack.pop() != '[')) {
-                return false;
-            }
-        }
-        if (!stack.isEmpty()) return false;
-        return true;
-
-        // Solution from Nick White
-        // if (s.length() % 2 != 0) return false;
-        
         // Stack<Character> stack = new Stack<Character>();
         // for (char c : s.toCharArray()) {
         //     if (c == '(' || c == '{' || c == '[') {
         //         stack.push(c);
         //     }
-        //     else if (c == ')' && !stack.isEmpty() && stack.peek() == '(') {
-        //         stack.pop();
+        //     if (c == ')' && (stack.isEmpty() || stack.pop() != '(')) {
+        //         return false;
         //     }
-        //     else if (c == '}' && !stack.isEmpty() && stack.peek() == '{') {
-        //         stack.pop();
+        //     if (c == '}' && (stack.isEmpty() || stack.pop() != '{')) {
+        //         return false;
         //     }
-        //     else if (c == ']' && !stack.isEmpty() && stack.peek() == '[') {
-        //         stack.pop();
+        //     if (c == ']' && (stack.isEmpty() || stack.pop() != '[')) {
+        //         return false;
         //     }
         // }
-        // return stack.isEmpty();
+        // if (!stack.isEmpty()) return false;
+        // return true;
+
+        // Solution from Nick White
+        if (s.length() % 2 != 0) return false;
+        
+        Stack<Character> stack = new Stack<Character>();
+        for (char c : s.toCharArray()) {
+            if (c == '(' || c == '{' || c == '[') {
+                stack.push(c);
+            }
+            else if (c == ')' && !stack.isEmpty() && stack.peek() == '(') {
+                stack.pop();
+            }
+            else if (c == '}' && !stack.isEmpty() && stack.peek() == '{') {
+                stack.pop();
+            }
+            else if (c == ']' && !stack.isEmpty() && stack.peek() == '[') {
+                stack.pop();
+            }
+            else {
+                return false;
+            }
+        }
+        return stack.isEmpty();
     }
 }
 // @lc code=end
