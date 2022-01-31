347ed8002c64d7f41b2a0621eaf3606557b201eb
2018yuli <l898974766@outlook.com>
2022-01-31 13:02:32 +0800

    20220131

---
+++ rust/hello-world-cargo/src/main.rs
@@ -0,0 +1,7 @@
+fn main() {
+    // The macro below prints 'hello world' to the console
+    /*
+     * multiline comment
+     */
+    println!("Hello, world!");
+}
---
+++ rust/variables/src/main.rs
@@ -0,0 +1,18 @@
+fn main() {
+    /*
+        let mut x = 45; // i32
+
+        println!("The value of x is {}", x);
+
+        // cannot assign twice to immutable variable
+        x = 60;
+
+        println!("The value of x is {}", x);
+    */
+    let x: i64 = 45;    // int64
+    let y: u64 = 45;    // 非负
+    let f: f32 = 6.7;
+    let b: bool = false;
+
+    println!("The values is {},{},{}, {}", x, y, f, b);
+}
