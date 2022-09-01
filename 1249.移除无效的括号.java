3eab6f421dd24d2a9551699fa9faa80df6ce2500
2018yuli <l898974766@outlook.com>
2022-09-01 20:31:39 +0800

    20220901

--- 1249.移除无效的括号.java
+++ 1249.移除无效的括号.java
@@ -8,33 +8,87 @@
 class Solution {
     public String minRemoveToMakeValid(String s) {
         // mine
-        List<Character> list = Arrays.asList(s.toCharArray());
+        // char[] charArray = s.toCharArray();
+        // List<Character> list = new ArrayList<Character>(s.length());
+        // for (char c : charArray) {
+        // 	list.add(c);
+        // }
 
-        Stack<Integer> stack = new Stack<Integer>();
+        // Stack<Integer> stack = new Stack<Integer>();
+        // List<Integer> mark = new ArrayList<Integer>();
 
-        for (int i = 0 ; i < list.size() ; i++) {
-            char c = list.get(i);
+        // for (int i = 0 ; i < list.size() ; i++) {
+        //     char c = list.get(i);
             
+        //     if (c == '(') {
+        //         stack.push(i);
+        //     }
+        //     if (c == ')') {
+        //         if (!stack.isEmpty()) {
+        //             stack.pop();
+        //         } else {
+        //             mark.add(i);
+        //         }
+        //     }
+        // }
+        // mark.addAll(stack);
+        // for (int i = 0 ; i < mark.size() ; i++) {
+        //     list.remove(mark.get(i) - i);
+        // }
+
+        // StringBuilder sb = new StringBuilder(list.size());
+        // for (char c : list) {
+        //     sb.append(c);
+        // }
+        // return sb.toString();
+
+        // mine2
+        // char[] charArray = s.toCharArray();
+        // Stack<Integer> stack = new Stack<Integer>();
+
+        // for (int i = 0 ; i < charArray.length ; i++) {
+        //     char c = charArray[i];
+            
+        //     if (c == '(') {
+        //         stack.push(i);
+        //     }
+        //     if (c == ')') {
+        //         if (stack.isEmpty())
+        //             charArray[i] = '-';
+        //         else
+        //             stack.pop();
+        //     }
+        // }
+        // while (!stack.isEmpty()) {
+        //     charArray[stack.pop()] = '-';
+        // }
+        // StringBuilder sb = new StringBuilder();
+        // for (char c : charArray) {
+        //     if (c != '-')
+        //         sb.append(c);
+        // }
+        // return sb.toString();
+
+        // Solution from NickWhite
+        StringBuilder sb = new StringBuilder();
+        int open = 0;
+        for (char c : s.toCharArray()) {
             if (c == '(') {
-                stack.push(i);
+                open++;
+            } else if (c == ')') {
+                if (open == 0)
+                    continue;
+                open--;
             }
-            if (c == ')') {
-                if (!stack.isEmpty()) {
-                    stack.pop();
-                } else {
-                    list.remove(i);
-                    i--;
-                }
-            }
-        }
-        while (!stack.isEmpty()) {
-            list.remove(stack.pop());
-        }
-        StringBuilder sb = new StringBuilder(list.size());
-        for (char c : list) {
             sb.append(c);
         }
-        return sb.toString();
+        StringBuilder ret = new StringBuilder();
+        for (int i = sb.length() - 1 ; i >= 0 ; i--) {
+            if (sb.charAt(i) == '(' && open -- > 0) 
+                continue;
+            ret.append(sb.charAt(i));
+        }
+        return ret.reverse().toString();
     }
 }
 // @lc code=end
